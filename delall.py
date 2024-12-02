a3c8d12437d7c3fd7c51de748f8816b65be402f3
realsonnyp <165358988+realsonnyp@users.noreply.github.com>
2024-12-02 09:40:00 -0500

    Update delall.py
    Added deletion comment

--- delall.py
+++ delall.py
@@ -2,3 +2,4 @@ def delvars():
     for variable in list(globals().keys()):
         if variable[0] != '_':  # Avoid deleting special variables
             del globals()[variable]
+print("All Jupyter variables deleted")
