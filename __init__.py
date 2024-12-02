1d570b36e11cdf1ac994cf6c98126f2a2cd276bc
realsonnyp <165358988+realsonnyp@users.noreply.github.com>
2024-12-02 07:29:40 -0500

    Update __init__.py
    Exposed(?) the init file

--- __init__.py
+++ __init__.py
@@ -1,9 +1,3 @@
-# delall_package/delall/__init__.py
+# /delall/__init__.py
 
-def delall():
-    """
-    Deletes all variables in the current namespace except for special ones (e.g., '__name__', '__doc__').
-    """
-    for name in list(globals().keys()):
-        if name[0] != '_':  # Avoid deleting special variables
-            del globals()[name]
+from .delall import delall  # Expose the delall function directly at the package level
